f253a0e7af4de1d1ecc7f3f918f2de26f9626f72
abhishek2chikun <abhishek2chikun@gmail.com>
2021-04-13 21:50:46 +0530

    First Commit

---
+++ logger.js
@@ -0,0 +1,14 @@
+var url ='http://mylogger.io/log';
+console.log(module)
+//console.log(__dirname)
+function x(message)
+{
+    //sent http request
+    console.log(message)
+}
+  
+
+
+module.exports=x;
+module.exports.Link=url;
+import os
